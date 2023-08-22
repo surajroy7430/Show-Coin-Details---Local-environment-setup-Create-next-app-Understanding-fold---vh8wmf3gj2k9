d2f4cc65ff51fc755adf9faabdd5768a020dd25c
Suraj Roy <surajroy7430@gmail.com>
2023-08-22 15:14:12 +0530

    1

--- app/components/CoinCard.js
+++ app/components/CoinCard.js
@@ -5,10 +5,10 @@ function CoinCard({ coin }) {
 
     return (
         <div className="coin-card">
-            <h3 className="coin-name">coin-name</h3>
-            <h4 className="coin-symbol">coin-symbol</h4>
-            <p className="coin-rank">Rank: coin-symbol</p>
-            <p className="coin-price">Price: $coin-price</p>
+            <h3 className="coin-name">{coin.name}</h3>
+            <h4 className="coin-symbol">{coin.symbol}</h4>
+            <p className="coin-rank">Rank: {coin.symbol}</p>
+            <p className="coin-price">Price: ${coin.price}</p>
             <Link className="view-details-link" href="">View Details</Link>
         </div>
     )
--- app/page.js
+++ app/page.js
@@ -5,12 +5,25 @@ import CoinCard from './components/CoinCard';
 function Home() {
     const [coins, setCoins] = useState([]);
 
+    const fetchCoins = async() => {
+        try {
+            const res = await fetch('https://api.coinlore.net/api/tickers/');
+            const coin = await res.json();
+            setCoins(coin.data.slice(0, 20));
+        } catch (error) {
+            console.log('Error: ', err);
+        }
+    }
+    useEffect(() => {
+        fetchCoins();
+    }, []);
+
     return (
         <div className='home'>
             <h1>Top 20 Cryptos</h1>
             <div className='coins-container'>
                 {coins.map((coin) => (
-                    <CoinCard />
+                    <CoinCard coin={coin} key={coin.id} />
                 ))}
             </div>
         </div>
