18efd12672f7c508af56eb1cecd36971d24768f8
Suraj Roy <surajroy7430@gmail.com>
2023-08-22 15:15:11 +0530

    1

--- app/[coinId]/page.js
+++ app/[coinId]/page.js
@@ -4,18 +4,31 @@ import React, { useState, useEffect } from 'react';
 function CoinDetail() {
     const [coin, setCoin] = useState(null);
 
+    const fetchCoinDetails = async() => {
+        try {
+            const res = await fetch(`https://api.coinlore.net/api/ticker/?id=${id}`);
+            const data = await res.json();
+            setCoin(data[0]);
+        } catch (error) {
+            console.log('Error: ', err);
+        }
+    }
+    useEffect(() => {
+        fetchCoinDetails();
+    }, []);
+
     return (
         <div className='coin-detail'>
             <div className='name-symbol'>
-                <h1 className='name'>coin-name</h1>
-                <h2 className='symbol'>(coin-symbol)</h2>
+                <h1 className='name'>{coin.name}</h1>
+                <h2 className='symbol'>({coin.symbol})</h2>
             </div>
             <div className='market-description'>
-                <p className='coin-rank'>Rank: coin-symbol</p>
-                <p className='coin-price'>Price: $coin-price</p>
-                <p className='coin-market-cap'>Market Cap: $coin-market_cap_usd</p>
-                <p className='coin-total-supply'>Total Supply: coin-tsupply</p>
-                <p className='coin-max-supply'>Max Supply: coin-msupply</p>
+                <p className='coin-rank'>Rank: {coin.symbol}</p>
+                <p className='coin-price'>Price: ${coin.price_usd}</p>
+                <p className='coin-market-cap'>Market Cap: ${coin.market_cap_usd}</p>
+                <p className='coin-total-supply'>Total Supply: {coin.tsupply}</p>
+                <p className='coin-max-supply'>Max Supply: {coin.msupply}</p>
             </div>
         </div>
     )
